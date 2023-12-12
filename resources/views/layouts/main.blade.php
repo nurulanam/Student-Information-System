2226fcd838c775ed6c69f9a749f97d90e718eb3e
Nurul Anam <nurulanam.bd@gmail.com>
2023-12-13 00:08:24 +0600

    Rem: Extra js and css files are deleted

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -35,13 +35,10 @@
       </div>
     </div>
   </div>
-  <script src="{{ asset('/assets/libs/jquery/dist/jquery.min.js') }}"></script>
-  <script src="{{ asset('/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('/assets/js/jquery.min.js') }}"></script>
+  <script src="{{ asset('/assets/js/bootstrap.bundle.min.js') }}"></script>
   <script src="{{ asset('/assets/js/sidebarmenu.js') }}"></script>
   <script src="{{ asset('/assets/js/app.min.js') }}"></script>
-  {{-- <script src="{{ asset('/assets/libs/apexcharts/dist/apexcharts.min.js') }}"></script> --}}
-  <script src="{{ asset('/assets/libs/simplebar/dist/simplebar.js') }}"></script>
-  {{-- <script src="{{ asset('/assets/js/dashboard.js') }}"></script> --}}
 
   @yield('extraJs')
 </body>
